7af58a4222e05481a222830f8b90adde950d0daf
Guilherme Avelino <gavelino@gmail.com>
2021-06-02 09:08:42 -0300

    new version

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          New App - Guilherme
+          New App - Guilherme v0.1
         </p>
         <a
           className="App-link"
